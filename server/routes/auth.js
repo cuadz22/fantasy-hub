6a469ab4fefcf43310c66cc6b19a87da19ecc7d7
Jose S Cuadros <cuadz@macbookpro.myfiosgateway.com>
2026-07-31 00:43:42 -0400

    add fspt-r scope back

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -38,7 +38,7 @@ router.get('/login', (req, res) => {
     client_id: CLIENT_ID,
     redirect_uri: REDIRECT_URI,
     response_type: 'code',
-    scope: 'openid',
+    scope: 'openid fspt-r',
   });
   res.redirect(`${YAHOO_AUTH_URL}?${params.toString()}`);
 });
